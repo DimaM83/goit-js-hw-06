45bb6e0c7d2838288528d321d2570b5f44731759
DimaM83 <mdv16.01.83@gmail.com>
2024-03-05 22:53:57 +0100

    fix #

--- js/task-2.js
+++ js/task-2.js
@@ -1,20 +1,20 @@
 class Storage {
   constructor(items) {
-    this._items = items;
+    this.#items = items;
   }
 
   getItems() {
-    return this._items;
+    return this.#items;
   }
 
   addItem(newItem) {
-    this._items.push(newItem);
+    this.#items.push(newItem);
   }
 
   removeItem(itemToRemove) {
-    const itemIndex = this._items.indexOf(itemToRemove);
+    const itemIndex = this.#items.indexOf(itemToRemove);
     if (itemIndex !== -1) {
-      this._items.splice(itemIndex, 1);
+      this.#items.splice(itemIndex, 1);
     }
   }
 }
